ab2a4332163e3b8dee225db1c5cdfce1acfba058
frans <frans.fuerst+git@gmail.com>
2016-05-30 19:16:48 +0200

    only connect on transition to 'standby'

--- reader_example.py
+++ reader_example.py
@@ -18,10 +18,13 @@ def test():
             context = zmq.Context()
             self._socket = context.socket(zmq.PUB)
             self._socket.bind("tcp://*:%d" % port)
+            self._output_file = open(time.strftime('%Y%m%d-%H%M%S.log'), 'w')
 
         def on_update(self, data):
+            if data['status'] != 'connected':
+                return
             _now = time.time()
-            if _now - self._last > .1:
+            if _now - self._last > .05:
                 msg = ('{"time": %.2f,'
                        ' "raw": %.4f,'
                        ' "meditation": %.4f,'
@@ -30,6 +33,8 @@ def test():
                            data['meditation'], data['attention']))
                 print(msg)
                 self._socket.send_string(msg)
+                self._output_file.write(msg)
+                self._output_file.write('\n')
                 self._last = _now
 
     try:
@@ -38,8 +43,14 @@ def test():
         logging.error(ex)
         print(glob.glob('/dev/ttyUSB*'))
         raise SystemExit(-1)
-    c.autoconnect()
+    old_status = None
     while True:
+        status = c.get_status()
+        if status == 'standby' and old_status != status:
+            c.autoconnect()
+            old_status = status
+        if not status == 'connected':
+            print(status)
         time.sleep(1)
 
 if __name__ == '__main__':
